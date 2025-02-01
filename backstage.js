049aa0af019f08c51f3369bce191478fafc2c4d9
i1li <74943880+i1li@users.noreply.github.com>
2025-02-01 13:13:49 -0600

    i

--- backstage.js
+++ backstage.js
@@ -152,6 +152,8 @@ binauralR=[rightCH1,rightCH2,rightCH3,rightCH4,rightCH5,rightCH6];
 flipBinauralF=[centerCH1,centerCH2,centerCH3,leftCH1,rightCH1,leftCH3,rightCH3,leftCH5,rightCH5];
 flipBinauralT=[centerCH1,centerCH2,centerCH3,leftCH2,rightCH2,leftCH4,rightCH4,leftCH6,rightCH6];flipBinauralF2=[leftCH1,rightCH1,leftCH3,rightCH3,leftCH5,rightCH5];
 flipBinauralT2=[leftCH2,rightCH2,leftCH4,rightCH4,leftCH6,rightCH6];
+flipBinauralF3=[centerCH2,centerCH3,leftCH1,rightCH1,leftCH3,rightCH3,leftCH5,rightCH5];
+flipBinauralT3=[centerCH2,centerCH3,leftCH2,rightCH2,leftCH4,rightCH4,leftCH6,rightCH6];
 stutterFadeCHs=[centerCH2,centerCH3,leftCH1,rightCH1,leftCH2,rightCH2,leftCH3,rightCH3,leftCH4,rightCH4,leftCH5,rightCH5,leftCH6,rightCH6];
 
 // midi cc 123 "all notes off" prevents sustain across transitions
