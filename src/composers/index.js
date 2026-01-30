00ab8a60e02bf9e33f0d4beba85fc8ad48e22566
i1li <74943880+i1li@users.noreply.github.com>
2026-01-29 18:56:09 -0600

    refactor(composers): extract Scale and Mode composers to src/composers and export via index

--- src/composers/index.js
+++ src/composers/index.js
@@ -1,5 +1,6 @@
 const MeasureComposer = require('./MeasureComposer');
 const { ScaleComposer, RandomScaleComposer } = require('./ScaleComposer');
 const { ChordComposer, RandomChordComposer } = require('./ChordComposer');
+const { ModeComposer, RandomModeComposer } = require('./ModeComposer');
 
-try { module.exports = { MeasureComposer, ScaleComposer, RandomScaleComposer, ChordComposer, RandomChordComposer }; } catch (e) { /* swallow */ }
+try { module.exports = { MeasureComposer, ScaleComposer, RandomScaleComposer, ChordComposer, RandomChordComposer, ModeComposer, RandomModeComposer }; } catch (e) { /* swallow */ }
