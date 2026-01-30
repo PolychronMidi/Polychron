7581219a9cd16d579a967ae1e803bdcf38bea3ec
i1li <74943880+i1li@users.noreply.github.com>
2026-01-29 19:01:05 -0600

    refactor(composers): extract pentatonic and mode composers to src/composers

---
+++ src/composers/PentatonicComposer.js
@@ -0,0 +1,86 @@
+const MeasureComposer = require('./MeasureComposer');
+const { writeDebugFile } = require('../debug/logGate');
+
+class PentatonicComposer extends MeasureComposer {
+  constructor(root = 'C', type = 'major') {
+    super();
+    this.root = root;
+    this.type = type;
+    this.noteSet(root, type);
+  }
+
+  noteSet(root, type = 'major') {
+    this.root = root;
+    this.type = type.toLowerCase();
+
+    const scaleName = this.type === 'minor' ? 'minor pentatonic' : 'major pentatonic';
+    this.scale = t.Scale.get(`${root} ${scaleName}`);
+    this.notes = this.scale.notes;
+
+    if (!this.notes || this.notes.length === 0) {
+      writeDebugFile('composers.ndjson', { tag: 'pentatonic-missing', root, type }, 'debug');
+      this.root = allNotes[ri(allNotes.length - 1)];
+      this.type = rf() < 0.5 ? 'major' : 'minor';
+      const fallbackScaleName = this.type === 'minor' ? 'minor pentatonic' : 'major pentatonic';
+      this.scale = t.Scale.get(`${this.root} ${fallbackScaleName}`);
+      this.notes = this.scale.notes;
+    }
+  }
+
+  getNotes(octaveRange = null) {
+    const [minOctave, maxOctave] = octaveRange || this.getOctaveRange();
+    const voices = this.getVoices();
+    const uniqueNotes = new Set();
+    const result = [];
+
+    const openIntervals = [0, 2, 4];
+
+    for (let i = 0; i < voices && i < this.notes.length; i++) {
+      const intervalIndex = openIntervals[i % openIntervals.length];
+      const noteIndex = intervalIndex % this.notes.length;
+      let octave = ri(minOctave, maxOctave);
+
+      if (i > 0 && voices > 2) {
+        octave = minOctave + Math.floor(i * (maxOctave - minOctave) / (voices - 1));
+      }
+
+      let note = t.Note.chroma(this.notes[noteIndex]) + 12 * octave;
+
+      let attempts = 0;
+      while (uniqueNotes.has(note) && attempts < 12) {
+        octave = ri(minOctave, maxOctave);
+        note = t.Note.chroma(this.notes[noteIndex]) + 12 * octave;
+        attempts++;
+      }
+
+      if (!uniqueNotes.has(note)) {
+        uniqueNotes.add(note);
+        result.push({ note });
+      }
+    }
+
+    return result;
+  }
+
+  x = () => this.getNotes();
+}
+
+class RandomPentatonicComposer extends PentatonicComposer {
+  constructor() {
+    super();
+    this.noteSet();
+  }
+
+  noteSet() {
+    const randomRoot = allNotes[ri(allNotes.length - 1)];
+    const randomType = rf() < 0.5 ? 'major' : 'minor';
+    super.noteSet(randomRoot, randomType);
+  }
+
+  x() {
+    this.noteSet();
+    return super.x();
+  }
+}
+
+try { module.exports = { PentatonicComposer, RandomPentatonicComposer }; } catch (e) { /* swallow */ }
--- src/composers/index.js
+++ src/composers/index.js
@@ -2,5 +2,6 @@ const MeasureComposer = require('./MeasureComposer');
 const { ScaleComposer, RandomScaleComposer } = require('./ScaleComposer');
 const { ChordComposer, RandomChordComposer } = require('./ChordComposer');
 const { ModeComposer, RandomModeComposer } = require('./ModeComposer');
+const { PentatonicComposer, RandomPentatonicComposer } = require('./PentatonicComposer');
 
-try { module.exports = { MeasureComposer, ScaleComposer, RandomScaleComposer, ChordComposer, RandomChordComposer, ModeComposer, RandomModeComposer }; } catch (e) { /* swallow */ }
+try { module.exports = { MeasureComposer, ScaleComposer, RandomScaleComposer, ChordComposer, RandomChordComposer, ModeComposer, RandomModeComposer, PentatonicComposer, RandomPentatonicComposer }; } catch (e) { /* swallow */ }
