4403df5c518db8965676ba4a0dc7ed93ac5d21b8
i1li <74943880+i1li@users.noreply.github.com>
2026-01-29 18:46:51 -0600

    refactor(composers): extract ScaleComposer and RandomScaleComposer to src/composers

---
+++ src/composers/ScaleComposer.js
@@ -0,0 +1,46 @@
+const MeasureComposer = require('./MeasureComposer');
+const { writeIndexTrace, writeDebugFile } = require('../debug/logGate');
+
+/**
+ * Composes notes from a specific scale.
+ * @extends MeasureComposer
+ */
+class ScaleComposer extends MeasureComposer {
+  /**
+   * @param {string} scaleName - e.g., 'major', 'minor'
+   * @param {string} root - e.g., 'C', 'D#'
+   */
+  constructor(scaleName,root) {
+    super();
+    this.root=root;
+    this.noteSet(scaleName,root);
+  }
+  /**
+   * Sets scale and extracts notes.
+   * @param {string} scaleName
+   * @param {string} root
+   */
+  noteSet(scaleName,root) {
+    this.scale=t.Scale.get(`${root} ${scaleName}`);
+    this.notes=this.scale.notes;
+  }
+  /** @returns {{note: number}[]} Scale notes */
+  x=()=>this.getNotes();
+}
+
+class RandomScaleComposer extends ScaleComposer {
+  constructor() {
+    super('','');
+    this.noteSet();
+  }
+  /** Randomly selects scale and root from venue.js data */
+  noteSet() {
+    const randomScale=allScales[ri(allScales.length - 1)];
+    const randomRoot=allNotes[ri(allNotes.length - 1)];
+    super.noteSet(randomScale,randomRoot);
+  }
+  /** @returns {{note: number}[]} Random scale notes */
+  x() { this.noteSet(); return super.x(); }
+}
+
+try { module.exports = { ScaleComposer, RandomScaleComposer }; } catch (e) { /* swallow */ }
--- src/composers/index.js
+++ src/composers/index.js
@@ -1,3 +1,4 @@
 const MeasureComposer = require('./MeasureComposer');
+const { ScaleComposer, RandomScaleComposer } = require('./ScaleComposer');
 
-try { module.exports = { MeasureComposer }; } catch (e) { /* swallow */ }
+try { module.exports = { MeasureComposer, ScaleComposer, RandomScaleComposer }; } catch (e) { /* swallow */ }
