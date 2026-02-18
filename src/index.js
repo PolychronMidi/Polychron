9b94ed8a55a036ce509c316f678e85e63b07072e
i1li <74943880+i1li@users.noreply.github.com>
2026-02-18 17:46:09 -0600

    r

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 require('./conductor');
-// @ts-ignore: side-effect module load
-require('./events');
 require('./utils');
 require('./rhythm');
 require('./time');
 require('./composers');
 require('./fx');
-// @ts-ignore: side-effect module load
-require('./channelCoherence');
-// @ts-ignore: side-effect module load
-require('./playNotesEmitPick');
-// @ts-ignore: side-effect module load
-require('./playNotes');
-// @ts-ignore: side-effect module load
-require('./microUnitAttenuator');
 require('./writer');
-// Explicitly include main in index require list so that file listings are comprehensive
-// (main is guarded and will not auto-run when required as a module)
-require('./main');
+require('./play');
