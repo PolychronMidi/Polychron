387a41d295f62ed57f5f3d4a320bad5e16e91302
i1li <74943880+i1li@users.noreply.github.com>
2026-02-18 16:49:31 -0600

    m

--- src/conductor/profiles/conductorConfig.js
+++ src/conductor/profiles/conductorConfig.js
@@ -97,6 +97,11 @@ ConductorConfig = (() => {
         source: 1.0,
         reflection: 1.0,
         bass: 1.0
+      },
+      ccRangeScale: {
+        source: { default: 1.0 },
+        reflection: { default: 1.0 },
+        bass: { default: 1.0 }
       }
     },
     noiseCanvas: {
@@ -128,7 +133,12 @@ ConductorConfig = (() => {
         balStep: 3,
         sideBias: [-14, 14],
         lBalMax: 46,
-        ccGroupScale: { source: 0.85, reflection: 0.9, bass: 0.85 }
+        ccGroupScale: { source: 0.85, reflection: 0.9, bass: 0.85 },
+        ccRangeScale: {
+          source: { default: 0.85, 74: 0.75, 91: 0.7, 92: 0.7, 93: 0.7, 94: 0.65, 95: 0.7 },
+          reflection: { default: 0.9, 91: 0.8, 92: 0.8, 93: 0.8, 94: 0.75, 95: 0.8 },
+          bass: { default: 0.82, 71: 0.75, 74: 0.8, 91: 0.68, 92: 0.68, 93: 0.68, 94: 0.62, 95: 0.68 }
+        }
       },
       noiseCanvas: { panRange: 45, sustainRange: [0.9, 1.1] },
       rhythmDrift: { burst: [0.3, 0.9], flurry: [0.2, 0.6] }
@@ -159,7 +169,12 @@ ConductorConfig = (() => {
         sideBias: [-30, 30],
         sideBiasStep: 3,
         lBalMax: 70,
-        ccGroupScale: { source: 1.2, reflection: 1.35, bass: 1.15 }
+        ccGroupScale: { source: 1.2, reflection: 1.35, bass: 1.15 },
+        ccRangeScale: {
+          source: { default: 1.2, 11: 1.25, 70: 1.2, 71: 1.2, 74: 1.3, 91: 1.25, 92: 1.25, 93: 1.25, 94: 1.3, 95: 1.2 },
+          reflection: { default: 1.35, 11: 1.4, 70: 1.35, 71: 1.35, 74: 1.45, 91: 1.55, 92: 1.55, 93: 1.55, 94: 1.6, 95: 1.45 },
+          bass: { default: 1.15, 5: 1.1, 70: 1.2, 71: 1.2, 74: 1.2, 91: 1.1, 92: 1.1, 93: 1.1, 94: 1.15, 95: 1.1 }
+        }
       },
       noiseCanvas: { panRange: 80, sustainRange: [0.7, 1.35] },
       rhythmDrift: { burst: [0.9, 2.3], flurry: [0.5, 1.4] }
@@ -189,7 +204,12 @@ ConductorConfig = (() => {
         balStep: 3,
         sideBias: [-18, 18],
         lBalMax: 50,
-        ccGroupScale: { source: 0.85, reflection: 1.2, bass: 0.8 }
+        ccGroupScale: { source: 0.85, reflection: 1.2, bass: 0.8 },
+        ccRangeScale: {
+          source: { default: 0.85, 70: 0.8, 71: 0.8, 74: 0.82, 91: 0.9, 92: 0.9, 93: 0.9, 94: 0.88, 95: 0.9 },
+          reflection: { default: 1.2, 70: 1.0, 71: 1.0, 74: 1.05, 91: 1.5, 92: 1.5, 93: 1.5, 94: 1.45, 95: 1.5 },
+          bass: { default: 0.8, 11: 0.75, 70: 0.75, 71: 0.75, 74: 0.78, 91: 0.72, 92: 0.72, 93: 0.72, 94: 0.7, 95: 0.72 }
+        }
       },
       noiseCanvas: { panRange: 50, sustainRange: [0.85, 1.3] },
       rhythmDrift: { burst: [0.4, 1.1], flurry: [0.2, 0.8] }
@@ -219,7 +239,12 @@ ConductorConfig = (() => {
         balStep: 5,
         sideBias: [-24, 24],
         lBalMax: 62,
-        ccGroupScale: { source: 1.15, reflection: 0.95, bass: 1.25 }
+        ccGroupScale: { source: 1.15, reflection: 0.95, bass: 1.25 },
+        ccRangeScale: {
+          source: { default: 1.15, 1: 1.2, 5: 1.2, 11: 1.2, 70: 1.25, 71: 1.25, 74: 1.3, 91: 0.85, 92: 0.85, 93: 0.85, 94: 0.82, 95: 0.85 },
+          reflection: { default: 0.95, 91: 0.8, 92: 0.8, 93: 0.8, 94: 0.78, 95: 0.8 },
+          bass: { default: 1.25, 11: 1.35, 72: 1.2, 73: 1.2, 74: 1.15, 91: 0.78, 92: 0.78, 93: 0.78, 94: 0.76, 95: 0.78 }
+        }
       },
       noiseCanvas: { panRange: 70, sustainRange: [0.75, 1.15] },
       rhythmDrift: { burst: [0.7, 1.8], flurry: [0.4, 1.2] }
@@ -250,7 +275,12 @@ ConductorConfig = (() => {
         sideBias: [-10, 10],
         sideBiasStep: 1,
         lBalMax: 40,
-        ccGroupScale: { source: 0.7, reflection: 0.75, bass: 0.7 }
+        ccGroupScale: { source: 0.7, reflection: 0.75, bass: 0.7 },
+        ccRangeScale: {
+          source: { default: 0.7, 70: 0.65, 71: 0.65, 74: 0.68, 91: 0.55, 92: 0.55, 93: 0.55, 94: 0.5, 95: 0.55 },
+          reflection: { default: 0.75, 70: 0.7, 71: 0.7, 74: 0.72, 91: 0.62, 92: 0.62, 93: 0.62, 94: 0.58, 95: 0.62 },
+          bass: { default: 0.68, 11: 0.65, 70: 0.62, 71: 0.62, 74: 0.64, 91: 0.5, 92: 0.5, 93: 0.5, 94: 0.48, 95: 0.5 }
+        }
       },
       noiseCanvas: { panRange: 32, sustainRange: [0.95, 1.05] },
       rhythmDrift: { burst: [0.2, 0.6], flurry: [0.1, 0.4] }
