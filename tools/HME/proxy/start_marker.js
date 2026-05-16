eac779f663826e7548ea6a08790e58831d6e748f
i1li <jgord38@wgu.edu>
2026-05-16 10:36:58 -0500

    2026-05-16T15:36:58

---
+++ tools/HME/proxy/start_marker.js
@@ -0,0 +1,11 @@
+use strict';
+
+function emitStartMarker(name, details = {}) {
+  const fields = Object.entries({ pid: process.pid, ...details })
+    .filter(([, value]) => value !== undefined && value !== '')
+    .map(([key, value]) => `${key}=${String(value).replace(/\s+/g, '_')}`)
+    .join(' ');
+  process.stderr.write(`=== ${name} start ${new Date().toISOString()} ${fields} ===\n`);
+}
+
+module.exports = { emitStartMarker };
