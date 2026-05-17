da0ad6eaeb01d30db0cedfa77164278a29f31f8e
i1li <jgord38@wgu.edu>
2026-05-17 12:07:30 -0500

    2026-05-17T12:07:30

--- tools/HME/proxy/hme_proxy_claude.js
+++ tools/HME/proxy/hme_proxy_claude.js
@@ -58,7 +58,7 @@ function createClaudeHandler(deps) {
     PROXY_STARTED_AT,
     routeMetrics: _routeMetrics,
     stopGateHealth: _stopGateHealth,
-    handleLifecycleRoute: _handleLifecycleRoute,
+    handleLifecycleRoute: (req, res) => lifecycleBridge().handleLifecycleRoute(req, res),
   });
 
   function handleRequest(clientReq, clientRes) {
