e2caba06233ec7a8a3e5b3f2979c8f8e6261db20
i1li <jgord38@wgu.edu>
2026-05-17 10:18:19 -0500

    2026-05-17T15:18:19

--- tools/HME/tests/specs/stop_gate_health.test.js
+++ tools/HME/tests/specs/stop_gate_health.test.js
@@ -4,6 +4,7 @@ const assert = require('node:assert/strict');
 const fs = require('node:fs');
 const os = require('node:os');
 const path = require('node:path');
+const { EventEmitter } = require('node:events');
 
 const repo = '/home/jah/Polychron';
 
