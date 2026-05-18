c26fffb3af3f3e51d347eb9551dd0021c0314e4b
i1li <jgord38@wgu.edu>
2026-05-18 17:25:56 -0500

    2026-05-18T22:25:56

--- tools/HME/tests/specs/session_read_cache.test.js
+++ tools/HME/tests/specs/session_read_cache.test.js
@@ -9,9 +9,9 @@ const REPO_ROOT = path.resolve(__dirname, '..', '..', '..', '..');
 
 function _isolate() {
   const tmp = fs.mkdtempSync(path.join(os.tmpdir(), 'hme-readcache-'));
-  process.env.HME_RUNTIME_DIR = tmp;
+  process.env.HME_SESSION_READ_CACHE_DIR = tmp;
   for (const k of Object.keys(require.cache)) {
-    if (k.includes('/tools/HME/proxy/session_read_cache') || k.includes('/tools/HME/proxy/hme_paths') || k.includes('/tools/HME/proxy/sse_rewriters')) delete require.cache[k];
+    if (k.includes('/tools/HME/proxy/session_read_cache') || k.includes('/tools/HME/proxy/sse_rewriters')) delete require.cache[k];
   }
   return tmp;
 }
