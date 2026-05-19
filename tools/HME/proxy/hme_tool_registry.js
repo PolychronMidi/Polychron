5c88697ce5d5985e80b9fa1616a407bafe6f146b
i1li <jgord38@wgu.edu>
2026-05-19 15:17:58 -0500

    2026-05-19T20:17:58

--- tools/HME/proxy/hme_tool_registry.js
+++ tools/HME/proxy/hme_tool_registry.js
@@ -36,8 +36,41 @@ function canonicalToolMetadata() {
   return JSON.parse(JSON.stringify(cachedMeta));
 }
 
+function canonicalToolMetadataByName() {
+  if (!cachedMetaByName) cachedMetaByName = Object.fromEntries(canonicalToolMetadata().map((tool) => [tool.name, tool]));
+  return JSON.parse(JSON.stringify(cachedMetaByName));
+}
+
+function canonicalRequiredFieldsByName() {
+  if (!cachedRequiredByName) {
+    cachedRequiredByName = Object.fromEntries(canonicalToolMetadata().map((tool) => [tool.name, (tool.parameters && tool.parameters.required) || []]));
+  }
+  return JSON.parse(JSON.stringify(cachedRequiredByName));
+}
+
 function canonicalToolNames() {
   return new Set(canonicalToolSchemas().map((tool) => tool.name));
 }
 
-module.exports = { canonicalToolSchemas, canonicalToolMetadata, canonicalToolNames };
+function toolMetadata(name) {
+  return canonicalToolMetadataByName()[name] || null;
+}
+
+function requiredFields(name) {
+  return canonicalRequiredFieldsByName()[name] || [];
+}
+
+function inputAliases(name) {
+  const meta = toolMetadata(name);
+  return meta && meta.hme && meta.hme.input_aliases && typeof meta.hme.input_aliases === 'object' ? meta.hme.input_aliases : {};
+}
+
+function missingRequiredFields(name, args = {}) {
+  const aliases = inputAliases(name);
+  return requiredFields(name).filter((field) => {
+    const keys = [field, ...((aliases[field] || []))];
+    return !keys.some((key) => Object.prototype.hasOwnProperty.call(args, key) && args[key] != null && String(args[key]).length > 0);
+  });
+}
+
+module.exports = { canonicalToolSchemas, canonicalToolMetadata, canonicalToolMetadataByName, canonicalRequiredFieldsByName, canonicalToolNames, toolMetadata, requiredFields, inputAliases, missingRequiredFields };
