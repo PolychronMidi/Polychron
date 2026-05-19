79de0535135580d03d18364dec18b286a654c2dc
i1li <jgord38@wgu.edu>
2026-05-19 10:00:29 -0500

    2026-05-19T15:00:29

--- tools/HME/proxy/codex_tool_loop.js
+++ tools/HME/proxy/codex_tool_loop.js
@@ -153,4 +153,4 @@ function followupBody(previousBody, responseBody, toolOutputs, events = []) {
   return body;
 }
 
-module.exports = { collectToolCalls, collectSseToolCalls, parseSseEvents, toolResultInput, followupBody, EMPTY_BASH_TOOL_RESULT };
+module.exports = { collectToolCalls, collectSseToolCalls, parseSseEvents, toolResultInput, followupBody, isIncompleteToolCall, EMPTY_BASH_TOOL_RESULT };
