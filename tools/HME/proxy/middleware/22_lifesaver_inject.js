4cb6c94e9d8b56cf4cf9b02cf236b196cd32db77
i1li <jgord38@wgu.edu>
2026-05-21 11:53:37 -0500

    2026-05-21T16:53:37

--- tools/HME/proxy/middleware/22_lifesaver_inject.js
+++ tools/HME/proxy/middleware/22_lifesaver_inject.js
@@ -14,6 +14,7 @@ const CANARY_RE = /\[CANARY-/;
 const OBSERVATION_RE = /\b(WARN|WARNING|INFO|DEBUG|NOTICE)\b/;
 const SELF_TAG_RE = /^\[(_safe_curl|_safe_jq|_safe_py3|universal_pulse|supervisor|hme-proxy|proxy-bridge|proxy-watchdog|proxy-supervisor|llamacpp_supervisor|llamacpp_offload_invariant|llamacpp_indexing_mode_resume|meta_observer|model_init|rag_proxy\.project|startup_chain|worker_client|worker:[^\]]+|hook-failure|sessionstart:[^\]]+)\]/;
 const HOOK_WATCHDOG_MISSING_RE = /^\[hook-watchdog\]\s+\[ALERT\]\s+UserPromptSubmit fired before successful SessionStart\.\s+\|\s+Session:\s+([0-9a-f]{6,12}|[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})\s*$/i;
+const HOOK_STOP_BLOCK_RE = /^\[hook-stop-block\]\s+Stop hook error:/i;
 const CRITICAL_INFRA_SELF_RE = /^\[(universal_pulse|supervisor)\].*\b(CRITICAL|FAIL|child_restart_limit|restart_limit|giving up|gave up|unhealthy|required)\b/i;
 
 function _isAgentActionable(line) {
