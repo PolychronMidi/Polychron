89471e03d2d4006e2acab14af691f96bc0e0d2e4
i1li <jgord38@wgu.edu>
2026-05-22 11:49:39 -0500

    2026-05-22T16:49:39

--- tools/HME/proxy/contexts/upstream_dispatch/index.js
+++ tools/HME/proxy/contexts/upstream_dispatch/index.js
@@ -1,7 +1,13 @@
 'use strict';
 
 const { createClaudeHandler } = require('../../hme_proxy_claude');
+const { omniProviderForConfigProvider } = require('../../omniroute_protocol');
+const { upstreamModelId } = require('../../overdrive_route');
+const swapStore = require('../../swap_state_store');
 
 module.exports = {
   createClaudeHandler,
+  omniProviderForConfigProvider,
+  upstreamModelId,
+  swapStore,
 };
