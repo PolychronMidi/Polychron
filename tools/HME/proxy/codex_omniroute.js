6d643fdc895d4c146d223f5d0bc56e53b86f65b5
i1li <jgord38@wgu.edu>
2026-05-22 11:56:30 -0500

    2026-05-22T16:56:30

--- tools/HME/proxy/codex_omniroute.js
+++ tools/HME/proxy/codex_omniroute.js
@@ -1,7 +1,6 @@
 'use strict';
 
-const { servicePort } = require('./service_registry');
-const resolver = require('./model_route_resolver');
+const { servicePort, resolver } = require('./contexts/upstream_dispatch');
 
 function targetChain(body, directUrl, loadConfig, env = process.env) {
   return resolver.codexTargetChain({ body, upstreamUrl: directUrl, cfg: loadConfig(), env, servicePort });
