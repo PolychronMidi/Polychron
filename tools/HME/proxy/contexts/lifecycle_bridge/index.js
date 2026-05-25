7f48ae5549941aafe7be91aaff06744608c000b6
i1li <jgord38@wgu.edu>
2026-05-25 13:53:32 -0500

    2026-05-25T18:53:32

--- tools/HME/proxy/contexts/lifecycle_bridge/index.js
+++ tools/HME/proxy/contexts/lifecycle_bridge/index.js
@@ -1,27 +1,21 @@
 'use strict';
 
-const lifecycleBridgeModule = require('../../lifecycle_bridge');
-const {
-  handleLifecycleRoute,
-  recordLifecycleHit,
-  lifecycleInactive,
-  runInlineFallback,
-} = lifecycleBridgeModule;
-const { emitStartMarker } = require('../../start_marker');
-const { createProxyRouteDispatcher } = require('../../hme_proxy_routes');
-const hmeDispatcher = require('../../hme_dispatcher');
-const supervisorChildren = require('../../supervisor/children');
-const supervisor = require('../../supervisor');
+function bridge(name) {
+  return (...args) => require('../../lifecycle_bridge')[name](...args);
+}
+function from(modulePath, name) {
+  return (...args) => require(modulePath)[name](...args);
+}
 
 module.exports = {
-  handleLifecycleRoute,
-  recordLifecycleHit,
-  lifecycleInactive,
-  runInlineFallback,
-  emitStartMarker,
-  createProxyRouteDispatcher,
-  lifecycleBridge: lifecycleBridgeModule,
-  hmeDispatcher,
-  supervisorChildren,
-  supervisor,
+  handleLifecycleRoute: bridge('handleLifecycleRoute'),
+  recordLifecycleHit: bridge('recordLifecycleHit'),
+  lifecycleInactive: bridge('lifecycleInactive'),
+  runInlineFallback: bridge('runInlineFallback'),
+  emitStartMarker: from('../../start_marker', 'emitStartMarker'),
+  createProxyRouteDispatcher: from('../../hme_proxy_routes', 'createProxyRouteDispatcher'),
+  get lifecycleBridge() { return require('../../lifecycle_bridge'); },
+  get hmeDispatcher() { return require('../../hme_dispatcher'); },
+  get supervisorChildren() { return require('../../supervisor/children'); },
+  get supervisor() { return require('../../supervisor'); },
 };
