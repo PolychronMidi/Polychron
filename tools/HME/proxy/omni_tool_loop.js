1948640b987fb3444c4107ebce7bdd6aadedfafa
i1li <jgord38@wgu.edu>
2026-05-25 09:05:23 -0500

    2026-05-25T14:05:23

--- tools/HME/proxy/omni_tool_loop.js
+++ tools/HME/proxy/omni_tool_loop.js
@@ -260,4 +260,4 @@ function _sseText(bodyStr) {
   return text.join('');
 }
 
-module.exports = { runToolLoop, extractToolUses, extractSseToolUses, parseSseEvents, MAX_TOOL_LOOP_DEPTH, EMPTY_BASH_TOOL_RESULT };
+module.exports = { runToolLoop, extractToolUses, extractSseToolUses, parseSseEvents, EMPTY_BASH_TOOL_RESULT };
