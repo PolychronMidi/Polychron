b53ccaf310e3d17f052ff21242c4ec20dbf03458
i1li <jgord38@wgu.edu>
2026-05-30 16:11:57 -0500

    2026-05-30T16:11:57

--- tools/HME/event_kernel/claude_adapter.js
+++ tools/HME/event_kernel/claude_adapter.js
@@ -5,6 +5,7 @@
 const fs = require('fs');
 const path = require('path');
 const { requireEnv } = require('../proxy/shared/load_env');
+const shortcutsConfig = require('../proxy/shortcuts_config');
 const { runHostAdapter, append } = require('./host_adapter_common');
 const { buildHostPayload, writeJsonAtomic } = require('./lifecycle_payload');
 const { claudeRelayFields, extractFirstJsonDocument } = require('./decision_normalizer');
