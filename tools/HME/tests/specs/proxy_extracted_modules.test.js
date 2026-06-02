c21cc4616b8c4edca5fb7e3a1a06e4557b6353f7
i1li <jgord38@wgu.edu>
2026-06-02 00:51:31 -0500

    2026-06-02T00:51:31

--- tools/HME/tests/specs/proxy_extracted_modules.test.js
+++ tools/HME/tests/specs/proxy_extracted_modules.test.js
@@ -1079,6 +1079,7 @@ test('context budget compaction gears start near context high-water and escalate
       }));
     };
     writeZeroStatusline();
+    process.env.HME_PROXY_ESTIMATOR_CALIBRATION = '0';
     process.env.HME_PROXY_CONTEXT_BYTES_PER_TOKEN_EST = '1';
     process.env.HME_PROXY_COMPACT_KEEP_MIN = '40';
     process.env.HME_PROXY_STALE_TOOL_KEEP_TURNS = '40';
