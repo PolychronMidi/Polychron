2e1aba1149aec9d71403be33ec081afd682f14bd
i1li <jgord38@wgu.edu>
2026-06-01 20:38:06 -0500

    2026-06-01T20:38:06

--- tools/HME/proxy/passthrough_compact.js
+++ tools/HME/proxy/passthrough_compact.js
@@ -100,6 +100,9 @@ function shrinkForPassthrough(payload, opts = {}) {
         keep_min: keepMin,
         max_tool_result_age: maxToolResultAge,
         tool_result_byte_floor: toolResultByteFloor,
+        target_tokens: Number(plan.targetTokens) || 0,
+        before_tokens: Number(plan.beforeTokens) || 0,
+        after_tokens: Number(plan.afterTokens) || 0,
       }, telemetry);
       return elided;
     }
