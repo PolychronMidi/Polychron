886c43841db91f4f2a50ff8d9ed7ffbc6205b6d6
i1li <jgord38@wgu.edu>
2026-06-02 22:54:39 -0500

    2026-06-02T22:54:39

--- tools/HME/proxy/hme_proxy_claude.js
+++ tools/HME/proxy/hme_proxy_claude.js
@@ -7,11 +7,11 @@ const {
   resolveUpstream, recordUpstreamFailure, isPassthroughMode,
 } = require('./contexts/upstream_dispatch');
 const { applyOverdriveRoute } = require('./contexts/upstream_dispatch');
-const { handleLegacySwapResponse } = require('./contexts/response_transform');
+const { handleLegacySwapResponse } = require('./legacy_swap_response');
 function handleAnthropicResponseComplete(...args) {
   // LAZY: avoids loading response/failure-policy stack while upstream_dispatch
-  // is still initializing hme_proxy_claude.
-  return require('./contexts/response_transform').handleAnthropicResponseComplete(...args);
+  // is still initializing hme_proxy_claude. Leaf import (not the
+  return require('./hme_proxy_anthropic_response').handleAnthropicResponseComplete(...args);
 }
 const middleware = require('./contexts/request_mutation').middleware;
 function mutateClaudeRequest(...args) {
