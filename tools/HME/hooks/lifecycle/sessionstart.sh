f7c4e3eb6bbffb5f79ce30239a92df5da1ca42b0
i1li <jgord38@wgu.edu>
2026-06-03 23:08:20 -0500

    2026-06-03T23:08:20

--- tools/HME/hooks/lifecycle/sessionstart.sh
+++ tools/HME/hooks/lifecycle/sessionstart.sh
@@ -251,7 +251,8 @@ _hme_bg_shell_timeout 20 list-carried-over "$PROJECT/log/hme-bg-list-carried-ove
 from todo_engine.lifesaver_bridge import list_carried_over
 from todo_engine import store
 store.recover_orphaned_carryovers()
-# store.load() applies due 2_/4f_ timer flips; archive only all-5_ sets.
+# store.load() applies due 2_/4f_ timer flips; archive when nothing is in
+# progress and >=1 item is 5_, carrying the non-5_ items into the next set.
 store.maybe_archive()
 items = list_carried_over()
 if items:
