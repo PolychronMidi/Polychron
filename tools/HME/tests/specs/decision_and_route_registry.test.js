75f105b5094808f9448d62da774a93b15f477658
i1li <jgord38@wgu.edu>
2026-06-04 11:12:47 -0500

    2026-06-04T11:12:47

--- tools/HME/tests/specs/decision_and_route_registry.test.js
+++ tools/HME/tests/specs/decision_and_route_registry.test.js
@@ -2,6 +2,7 @@
 const { test } = require('node:test');
 const assert = require('node:assert/strict');
 const path = require('node:path');
+const fs = require('node:fs');
 
 const Decision = require('../../event_kernel/decision');
 const Render = require('../../event_kernel/decision_renderer');
