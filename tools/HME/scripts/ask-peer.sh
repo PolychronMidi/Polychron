db542442dcab4fc15ffd4ab1cef02fc6c983f8a1
i1li <jgord38@wgu.edu>
2026-06-05 15:35:46 -0500

    2026-06-05T15:35:46

--- tools/HME/scripts/ask-peer.sh
+++ tools/HME/scripts/ask-peer.sh
@@ -98,7 +98,8 @@ RAW_CAP_FILE=""
 TRUNC_FILE=""
 RESP_FILE=""
 SID_OUT_FILE=""
-cleanup_tmp() { rm -f ${RAW_CAP_FILE:+"$RAW_CAP_FILE"} ${TRUNC_FILE:+"$TRUNC_FILE"} ${RESP_FILE:+"$RESP_FILE"} ${SID_OUT_FILE:+"$SID_OUT_FILE"}; }
+RAW_FIFO=""
+cleanup_tmp() { rm -f ${RAW_CAP_FILE:+"$RAW_CAP_FILE"} ${TRUNC_FILE:+"$TRUNC_FILE"} ${RESP_FILE:+"$RESP_FILE"} ${SID_OUT_FILE:+"$SID_OUT_FILE"} ${RAW_FIFO:+"$RAW_FIFO"}; }
 trap cleanup_tmp EXIT
 
 valid_sid() {
