00e2303212a76ed97488f73404abbfd543ef3824
i1li <jgord38@wgu.edu>
2026-06-07 22:36:59 -0500

    2026-06-07T22:36:59

--- tools/HME/proxy/incident_resolvers.js
+++ tools/HME/proxy/incident_resolvers.js
@@ -112,7 +112,7 @@ function _staleRuntime(line, root) {
   };
 }
 
-const RESOLVERS = [_observationOrSelf, _autocommitResolved, _upstreamContextWindow, _staleRuntime];
+const RESOLVERS = [_observationOrSelf, _autocommitResolved, _upstreamContextWindow, _upstreamTransient200ApiError, _staleRuntime];
 
 function resolveLine(root, line) {
   for (const r of RESOLVERS) {
