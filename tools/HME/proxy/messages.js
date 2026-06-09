a2851c2f30fb0685015e598d0df90b52b7ba6be6
i1li <jgord38@wgu.edu>
2026-06-08 21:20:58 -0500

    2026-06-08T21:20:58

--- tools/HME/proxy/messages.js
+++ tools/HME/proxy/messages.js
@@ -2,8 +2,6 @@
 // Message processing: boilerplate strip, semantic-redundancy strip, scan.
 
 const fs = require('fs');
-const os = require('os');
-const fsRealPath = fs.realpathSync;
 const path = require('path');
 const { emit, PROJECT_ROOT } = require('./shared');
 const { isJurisdictionFile } = require('./context');
