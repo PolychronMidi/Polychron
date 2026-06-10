720458c2a7da3b7f1306d1a939cf660e59bdcdd5
i1li <jgord38@wgu.edu>
2026-06-09 20:12:56 -0500

    2026-06-09T20:12:56

--- tools/HME/hooks/helpers/_safety.sh
+++ tools/HME/hooks/helpers/_safety.sh
@@ -64,7 +64,8 @@ source "${_HME_SAFETY_DIR}/latency.sh"
 source "${_HME_SAFETY_DIR}/misc_safe.sh"
 source "${_HME_SAFETY_DIR}/curl.sh"
 source "${_HME_SAFETY_DIR}/emitters.sh"
-source "${_HME_SAFETY_DIR}/http.sh"
+source "${_HME_SAFETY_DIR}/streak.sh"
+source "${_HME_SAFETY_DIR}/http.sh"}无码?
 
 # 6) Install EXIT trap AFTER _hme_exit_combined is defined (latency.sh).
 trap _hme_exit_combined EXIT
